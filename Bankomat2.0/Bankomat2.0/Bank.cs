d4865dfce21447934d72d57fe668df4696c4b6fa
albinaa07 <albina.abdulina@hotmail.com>
2015-09-28 10:26:22 +0200

    Bank klass skelett

---
+++ Bankomat2.0/Bankomat2.0/Bank.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Bankomat2._0
+{
+    public class Bank
+    {
+        //Bank identification code
+        string bic;
+        // Bank namn
+        string name;
+
+        public void ConductTransaction()
+        {
+
+        }
+
+        public void LogEvent()
+        {
+
+        }
+
+        public void GetPerson(string SSN)
+        {
+
+        }
+    }
+}
