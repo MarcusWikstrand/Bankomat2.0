e103e8fd9dfd11e139d04a3817fc57cb2deed9c1
MarcusWikstrand <marcus_wikstrand@hotmail.com>
2015-09-28 10:24:11 +0200

    Added Customer (originally Person), PaymentCard and Account.
    The account has MakeTransaction(decimal amount) aswell, creating a new transaction with a given amount.

---
+++ Bankomat2.0/Bankomat2.0/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Bankomat2._0
+{
+    public class Customer
+    {
+        public string ssn { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        
+
+    }
+}
