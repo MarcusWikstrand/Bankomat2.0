2631869174dec8225fc8b3b1ac60374f13cbed33
Ruckk <cr930326@gmail.com>
2015-09-29 13:28:27 +0200

    Added databaseConnection class.

---
+++ Bankomat2.0/Tester/Program.cs
@@ -0,0 +1,32 @@
+﻿using Bankomat2._0;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Tester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Run();
+        }
+
+        private static void Run()
+        {
+            ATM atm = new ATM(1337);
+
+            bool authResult = atm.Authenticate("51425555555", 1111);
+            Console.WriteLine(@"Login attempt, Outcome: {authResult}");
+
+            if (authResult == true)
+            {
+                Console.WriteLine("Trying to withdraw 100:-");
+                atm.Withdraw(100);
+            }
+        }
+    }
+}
